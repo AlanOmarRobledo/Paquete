fc9e2b4a47de2530ae97f64c85b6265f9297531b
Alan Robledo <47234164+AlanOmarRobledo@users.noreply.github.com>
2019-02-08 10:06:39 -0800

    Update Pruebas.java

--- Paquete/src/test/Pruebas.java
+++ Paquete/src/test/Pruebas.java
@@ -6,9 +6,10 @@ public class Pruebas {
 		// TODO Auto-generated method stub
 		paquete1.persona p = new paquete1.persona(20,"Javier");
 		paquete2.persona m = new paquete2.persona(18,"Luis","Masculino");
-		
+		System.out.println("Inicio de impresion");
 		System.out.println(p.toString());
 		System.out.println(m.toString());
+		System.out.println("Fin");
 
 	}
 
