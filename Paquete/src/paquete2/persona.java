db3fedc1659695e8bdcac134d95423d4a8e113a4
AlanOmarRobledo <alan.omar.robledo.lopez@uabc.edu.mx>
2019-02-08 09:56:00 -0800

    Prueba

---
+++ Paquete/src/paquete2/persona.java
@@ -0,0 +1,34 @@
+package paquete2;
+
+public class persona {
+	private int edad;
+	private String nombre;
+	private String genero;
+	
+	public persona(int edad, String nombre, String genero) {
+		super();
+		this.edad = edad;
+		this.nombre = nombre;
+		this.genero = genero;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+}
---
+++ Paquete/src/test/Pruebas.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		paquete1.persona p = new paquete1.persona(20,"Javier");
+		paquete2.persona m = new paquete2.persona(18,"Luis","Masculino");
+		
+		System.out.println(p.toString());
+		System.out.println(m.toString());
+
+	}
+
+}
